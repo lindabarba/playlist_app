f2ba196df3a46cca71613e6158cd06c633198530
Linda Barba <lindabarba13@gmail.com>
2017-04-20 19:30:53 -0700

    Add authentication and authorization

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 end
 
 
