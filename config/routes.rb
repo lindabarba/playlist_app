b13d7ad73732ea913f0364f1d9a563ffd557e6a3
Linda Barba <lindabarba13@gmail.com>
2017-04-24 14:47:21 -0700

    add controllers routes views for new models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,40 @@
 Rails.application.routes.draw do
+  get 'tags/index'
+
+  get 'tags/new'
+
+  get 'tags/create'
+
+  get 'tags/edit'
+
+  get 'tags/update'
+
+  get 'tags/destroy'
+
+  get 'songs/index'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/destroy'
+
+  get 'playlists/index'
+
+  get 'playlists/new'
+
+  get 'playlists/create'
+
+  get 'playlists/edit'
+
+  get 'playlists/update'
+
+  get 'playlists/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users
